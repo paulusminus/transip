cf9f1aa0177c077ac6ec2452c7b7f3809eef3e9a
paulusminus <info@paulmin.nl>
2023-10-21 12:02:49 +0200

    Remove setting env

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export RUST_LOG=info
 export CERTBOT_DOMAIN=paulmin.nl
 export CERTBOT_ALL_DOMAINS=paulmin.nl,paulmin.nl
 export CERTBOT_AUTH_OUTPUT=Ok
--- ping.sh
+++ ping.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-export RUST_LOG=info
-
 /usr/bin/transip-ping
