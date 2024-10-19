d7ca27434fbab5dbc3b3b1eda602ef5e2e9f57e8
paulusminus <info@paulmin.nl>
2024-10-19 19:24:02 +0200

    chore: update dependencies

--- crates/http-client/build.old.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    for (key, value) in std::env::vars() {
-        if key.starts_with("CARGO_CFG_TARGET_") {
-            println!("{key}: {value}");
-        }
-    }
-    // panic!("Just print the build environment");
-}
